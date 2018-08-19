276c2c3d69eba1a11113cfb3cebf775cfc38f039
Han Sung Bae <hansungbae@Hans-MacBook-Air.local>
2018-08-18 22:19:49 -0400

    heroku config added

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,16 @@
 var mysql = require("mysql");
+var connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWS_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'hacktheplanet',
+        database: 'todoagain_db'
+    });
+};
 
 var connection = mysql.createConnection({
     host: "localhost",
